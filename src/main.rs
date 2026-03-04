46e2b2de6b5d6c932d6b6d79ecf083775057887f
Jashanpreet singh <sjass6789@gmail.com>
2026-03-04 10:56:25 +0530

    Inital Commit

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+mod utils;
+mod routes;
+use actix_web::{App, HttpServer, middleware::Logger};
+
+
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+
+    if std::env::var_os("RUST_LOG").is_none() {
+        unsafe {
+            std::env::set_var("RUST_LOG","actix_web=info");
+        }
+
+    }
+    dotenv::dotenv().ok();
+    env_logger::init();
+    let port = (*utils::constants::PORT).clone();
+    let address = (*utils::constants::ADDRESS).clone();
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .configure(routes::home_routes::config)
+    })
+        .bind((address, port))?
+        .run()
+        .await
+}
